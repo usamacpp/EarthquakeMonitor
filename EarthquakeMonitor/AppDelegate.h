e84b460d314a4f85224d8f367590b7093e3c7e46
osama mourad <osamamikhail@osamas-MacBook-Air.local>
2014-12-13 12:45:52 +0200

    Initial Commit

---
+++ EarthquakeMonitor/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  EarthquakeMonitor
+//
+//  Created by osama mourad on 12/13/14.
+//  Copyright (c) 2014 osama mourad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ EarthquakeMonitor/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EarthquakeMonitor
+//
+//  Created by osama mourad on 12/13/14.
+//  Copyright (c) 2014 osama mourad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
